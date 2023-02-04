15ddda2886a62f612f0e4d189a54abde0eb62111
Michael Wright <beamer1989@gmail.com>
2023-02-04 13:47:57 -0800

    you can now see it being held

--- Assets/Scripts/character_movement.cs
+++ Assets/Scripts/character_movement.cs
@@ -7,7 +7,7 @@ public class character_movement : MonoBehaviour
 
     private float startPosX;
     private float startPosY;
-    private bool isBeingHeld = false;
+    public bool isBeingHeld = false;
 
     // Start is called before the first frame update
     void Start()
