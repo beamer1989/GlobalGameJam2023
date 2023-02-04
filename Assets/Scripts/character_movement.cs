7d407d9636f1612323f18b2ad50d31a727d4ccac
Michael Wright <beamer1989@gmail.com>
2023-02-03 23:22:54 -0800

    added line drawing to main character and movement

--- Assets/Scripts/character_movement.cs
+++ Assets/Scripts/character_movement.cs
@@ -24,7 +24,10 @@ public class character_movement : MonoBehaviour
             mousePos = Input.mousePosition;
             mousePos = Camera.main.ScreenToWorldPoint(mousePos);
 
-            this.gameObject.transform.Find("Shoot_cursor").gameObject.transform.position = new Vector2(mousePos.x, mousePos.y);
+            this.gameObject.transform.Find("ShootCursor").gameObject.transform.position = new Vector2(mousePos.x, mousePos.y);
+            this.gameObject.transform.Find("ShootingLine").gameObject.GetComponent<LineRenderer>().SetPosition(0,this.gameObject.transform.position);
+            this.gameObject.transform.Find("ShootingLine").gameObject.GetComponent<LineRenderer>().SetPosition(1,mousePos);
+
             
             
         }
@@ -40,7 +43,8 @@ public class character_movement : MonoBehaviour
             mousePos = Camera.main.ScreenToWorldPoint(mousePos);
 
             isBeingHeld = true;
-        
+            this.gameObject.transform.Find("ShootingLine").gameObject.GetComponent<LineRenderer>().enabled = true;
+            
         }
 
     }
@@ -48,13 +52,14 @@ public class character_movement : MonoBehaviour
     private void OnMouseUp()
     {
         isBeingHeld = false;
-        float x_force = this.gameObject.transform.position.x - this.gameObject.transform.Find("Shoot_cursor").gameObject.transform.position.x;
-        float y_force = this.gameObject.transform.position.y - this.gameObject.transform.Find("Shoot_cursor").gameObject.transform.position.y;
-        this.gameObject.transform.Find("Shoot_cursor").gameObject.transform.position = this.gameObject.transform.position;
+        float x_force = this.gameObject.transform.position.x - this.gameObject.transform.Find("ShootCursor").gameObject.transform.position.x;
+        float y_force = this.gameObject.transform.position.y - this.gameObject.transform.Find("ShootCursor").gameObject.transform.position.y;
+        this.gameObject.transform.Find("ShootCursor").gameObject.transform.position = this.gameObject.transform.position;
         Vector2 shoot_force = new Vector2(x_force, y_force);
         this.gameObject.GetComponent<Rigidbody2D>().AddForce(shoot_force, ForceMode2D.Impulse);
 
         print(x_force);
         print(y_force);
+        this.gameObject.transform.Find("ShootingLine").gameObject.GetComponent<LineRenderer>().enabled = false;
     }
 }
