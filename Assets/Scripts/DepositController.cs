8ac94bc820fcfec8f0abe1f2c9c2e4bd8ad696c5
Bryan Tai <bryan.tai@alumni.ubc.ca>
2023-02-04 13:18:40 -0800

    Added DepositPoint

--- Assets/Scripts/DepositController.cs
+++ Assets/Scripts/DepositController.cs
@@ -1,18 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DepositController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private void OnTriggerEnter2D(Collider2D col)
+	{
+		CharacterElementData playerController = col.gameObject.GetComponent<CharacterElementData>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		if (playerController == null || playerController.CanPickUpElement())
+		{
+			return;
+		}
+
+		playerController.CurrentElementState = ElementState.Default;
+	}
 }
