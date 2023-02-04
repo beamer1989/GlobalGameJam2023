66dcad7ed2c7526b08040d50866785979e8beb6d
Michael Wright <beamer1989@gmail.com>
2023-02-04 00:10:56 -0800

    added character rotation on launch and cleaned up code

--- Assets/Scripts/character_movement.cs
+++ Assets/Scripts/character_movement.cs
@@ -20,10 +20,12 @@ public class character_movement : MonoBehaviour
     {
         if(isBeingHeld == true)
         {
+            // get the position of the mouse
             Vector3 mousePos;
             mousePos = Input.mousePosition;
             mousePos = Camera.main.ScreenToWorldPoint(mousePos);
 
+            // set the position of the shooting line and cursor
             this.gameObject.transform.Find("ShootCursor").gameObject.transform.position = new Vector2(mousePos.x, mousePos.y);
             this.gameObject.transform.Find("ShootingLine").gameObject.GetComponent<LineRenderer>().SetPosition(0,this.gameObject.transform.position);
             this.gameObject.transform.Find("ShootingLine").gameObject.GetComponent<LineRenderer>().SetPosition(1,mousePos);
@@ -37,13 +39,13 @@ public class character_movement : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0))
         {
-
-            Vector3 mousePos;
-            mousePos = Input.mousePosition;
-            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-
+            
+            // set mouse to being held
             isBeingHeld = true;
+
+            // enable visibility of shooting line and cursor
             this.gameObject.transform.Find("ShootingLine").gameObject.GetComponent<LineRenderer>().enabled = true;
+            this.gameObject.transform.Find("ShootCursor").gameObject.GetComponent<SpriteRenderer>().enabled = true;
             
         }
 
@@ -51,15 +53,30 @@ public class character_movement : MonoBehaviour
 
     private void OnMouseUp()
     {
+        // release mouse held
         isBeingHeld = false;
+
+        // get force to send character
         float x_force = this.gameObject.transform.position.x - this.gameObject.transform.Find("ShootCursor").gameObject.transform.position.x;
         float y_force = this.gameObject.transform.position.y - this.gameObject.transform.Find("ShootCursor").gameObject.transform.position.y;
+        Vector2 shoot_force = new Vector2(x_force, y_force);    
+
+        // get rotation to send character
+        Vector3 diff = this.gameObject.transform.Find("ShootCursor").gameObject.transform.position - this.gameObject.transform.position;
+        diff.Normalize();
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+
+        // return target cursor to character
         this.gameObject.transform.Find("ShootCursor").gameObject.transform.position = this.gameObject.transform.position;
-        Vector2 shoot_force = new Vector2(x_force, y_force);
+
+        // apply force and rotation to character
+        this.gameObject.transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
         this.gameObject.GetComponent<Rigidbody2D>().AddForce(shoot_force, ForceMode2D.Impulse);
 
-        print(x_force);
-        print(y_force);
+        // remove the shooting line and cursor
         this.gameObject.transform.Find("ShootingLine").gameObject.GetComponent<LineRenderer>().enabled = false;
+        this.gameObject.transform.Find("ShootCursor").gameObject.GetComponent<SpriteRenderer>().enabled = false;
+
+
     }
 }
