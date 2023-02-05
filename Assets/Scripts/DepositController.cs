dd90ccafefd8ec67b245112d0456ce2868131aac
Bryan Tai <bryan.tai@alumni.ubc.ca>
2023-02-04 22:35:20 -0800

    Added victory condition

--- Assets/Scripts/DepositController.cs
+++ Assets/Scripts/DepositController.cs
@@ -2,9 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DepositController : MonoBehaviour
 {
+	public int TotalCollectables = 0;
+
+	private int _currentTotalCollected = 0;
+	
 	private void OnTriggerEnter2D(Collider2D col)
 	{
 		CharacterElementData playerController = col.gameObject.GetComponent<CharacterElementData>();
@@ -19,6 +24,19 @@ public class DepositController : MonoBehaviour
 		col.gameObject.GetComponent<PlaySoundEffects>().playDeliver();
                 
         StrokeCounter.IncreaseStrokes();
+        
+		CheckIfGameWin();
+	}
+	
+	private void CheckIfGameWin()
+	{
+		_currentTotalCollected++;
+
+		if (_currentTotalCollected >= TotalCollectables)
+		{
+			//Trigger game win
+			SceneManager.LoadScene(4);
+		}
 	}
 
 
---
+++ Assets/Scripts/UGUIVictoryScreen.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UGUIVictoryScreen : MonoBehaviour
+{
+    public void NewGameButtonPressed()
+    {
+        Debug.Log("Continue Button Pressed");
+        SceneManager.LoadScene(1); // hardcoded scene id for now
+    }
+    public void QuitButtonPressed()
+    {
+        Debug.Log("Quit Button Pressed, exiting game.");
+        Application.Quit();
+    }
+}
