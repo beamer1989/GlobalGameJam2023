82920919c6e2b453642c8a29a4dcd8abeb3de463
Sarah Tachibana <sparou@gmail.com>
2023-02-05 13:54:05 -0800

    Add loading notification to Title Screen

--- Assets/Scripts/UGUITitleScreen.cs
+++ Assets/Scripts/UGUITitleScreen.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UGUITitleScreen : MonoBehaviour
 {
+    public Text loading;
+
+    public void Start()
+    {
+        loading.enabled = false;
+    }
     public void NewGameButtonPressed()
     {
         Debug.Log("New Game Button Pressed");
+        loading.enabled = true;
         SceneManager.LoadScene(1); // hardcoded scene id for now
     }
     public void TutorialButtonPressed()
     {
         Debug.Log("Tutorial Button Pressed");
+        loading.enabled = true;
         SceneManager.LoadScene(2); // hardcoded scene id for now
     }
     public void QuitButtonPressed()
